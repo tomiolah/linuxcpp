9c1a5157749a27a4c0a8dd50e93a2a909e9cf9b5
Tomi Olah <tomiolah1998@gmail.com>
2019-02-02 16:19:09 +0200

    Added colors

---
+++ colors.hpp
@@ -0,0 +1,98 @@
+#ifndef __COLORS_HPP__
+#define __COLORS_HPP__
+
+
+// DEFINE
+    #define RESET "\033[0m"
+
+    // STANDRAD
+        #define BLACK  "\033[30;0m"
+        #define RED    "\033[31;0m"
+        #define GREEN  "\033[32;0m"
+        #define YELLOW "\033[33;0m"
+        #define BLUE   "\033[34;0m"
+        #define PURPLE "\033[35;0m"
+        #define CYAN   "\033[36;0m"
+        #define WHITE  "\033[37;0m"
+
+    // BOLD
+        #define BBLACK  "\033[30;1m"
+        #define BRED    "\033[31;1m"
+        #define BGREEN  "\033[32;1m"
+        #define BYELLOW "\033[33;1m"
+        #define BBLUE   "\033[34;1m"
+        #define BPURPLE "\033[35;1m"
+        #define BCYAN   "\033[36;1m"
+        #define BWHITE  "\033[37;1m"
+
+    // UNDERLINE
+        #define UBLACK  "\033[30;4m"
+        #define URED    "\033[31;4m"
+        #define UGREEN  "\033[32;4m"
+        #define UYELLOW "\033[33;4m"
+        #define UBLUE   "\033[34;4m"
+        #define UPURPLE "\033[35;4m"
+        #define UCYAN   "\033[36;4m"
+        #define UWHITE  "\033[37;4m"
+
+    // BACKGROUND
+        #define BACK_BLACK  "\033[40m"
+        #define BACK_RED    "\033[41m"
+        #define BACK_GREEN  "\033[42m"
+        #define BACK_YELLOW "\033[43m"
+        #define BACK_BLUE   "\033[44m"
+        #define BACK_PURPLE "\033[45m"
+        #define BACK_CYAN   "\033[46m"
+        #define BACK_WHITE  "\033[47m"
+
+// -----------------------------
+
+/*
+    // STRING
+        char reset[5] = "\033[0m"
+
+        // STANDRAD
+            char black  [8] = "\033[30;0m";
+            char red    [8] = "\033[31;0m";
+            char green  [8] = "\033[32;0m";
+            char yellow [8] = "\033[33;0m";
+            char blue   [8] = "\033[34;0m";
+            char purple [8] = "\033[35;0m";
+            char cyan   [8] = "\033[36;0m";
+            char white  [8] = "\033[37;0m";
+
+
+        // BOLD
+            char bblack  [8] = "\033[30;1m";
+            char bred    [8] = "\033[31;1m";
+            char bgreen  [8] = "\033[32;1m";
+            char byellow [8] = "\033[33;1m";
+            char bblue   [8] = "\033[34;1m";
+            char bpurple [8] = "\033[35;1m";
+            char bcyan   [8] = "\033[36;1m";
+            char bwhite  [8] = "\033[37;1m";
+
+
+        // UNDERLINE
+            char ublack  [8] = "\033[30;4m";
+            char ured    [8] = "\033[31;4m";
+            char ugreen  [8] = "\033[32;4m";
+            char uyellow [8] = "\033[33;4m";
+            char ublue   [8] = "\033[34;4m";
+            char upurple [8] = "\033[35;4m";
+            char ucyan   [8] = "\033[36;4m";
+            char uwhite  [8] = "\033[37;4m";
+
+
+        // BACKGROUND
+            char back_black  [6] = "\033[40m";
+            char back_red    [6] = "\033[41m";
+            char back_green  [6] = "\033[42m";
+            char back_yellow [6] = "\033[43m";
+            char back_blue   [6] = "\033[44m";
+            char back_purple [6] = "\033[45m";
+            char back_cyan   [6] = "\033[46m";
+            char back_white  [6] = "\033[47m";
+*/
+
+#endif
